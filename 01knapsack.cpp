032e55b26d314ae45490f17590af7c195d1ca5b8
ShinMeow <89983831+SyncMeow@users.noreply.github.com>
2022-11-09 19:30:28 +0800

    Update 01knapsack.cpp

--- 01knapsack.cpp
+++ 01knapsack.cpp
@@ -1,25 +1,20 @@
-//ZeroJudge b184
 #include <bits/stdc++.h>
+#define ilovekanade ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
 
-const int limit = 100;
-
 int main(){
-	int n;
-	while (cin >> n){
-		int v[105], w[105], dp[105][105];
-		for(int i=0;i<n;i++) cin >> w[i] >> v[i];
+    int n, limit, dp[200][300]; cin >> n >> limit;
 
-		memset(dp, 0, sizeof(dp));
+    vector<int> w(n), v(n);
+    
+    for(int i = 0;i < n;i++) cin >> w[i] >> v[i];
 
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j <= limit; j++){
-				if (j < w[i]) dp[i+1][j] = dp[i][j];
-				else dp[i+1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
-			}
-		}
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j <= limit; j++){
+            if(j < w[i]) dp[i+1][j] = dp[i][j];
+            else dp[i+1][j] = max(dp[i][j], v[i] + dp[i][j-w[i]]);
+        }
+    }
 
-		cout << dp[n][limit] << "\n";
-	}
-	
+    cout << dp[n][limit];
 }
