0329aa37eed5c417f936930f02524a7ef1300f0a
ShinMeow <89983831+SyncMeow@users.noreply.github.com>
2022-11-09 19:22:01 +0800

    Update 01knapsack.cpp

--- 01knapsack.cpp
+++ 01knapsack.cpp
@@ -8,18 +8,18 @@ int main(){
 	int n;
 	while (cin >> n){
 		int v[105], w[105], dp[105][105];
-			for(int i=0;i<n;i++) cin >> w[i] >> v[i];
+		for(int i=0;i<n;i++) cin >> w[i] >> v[i];
 
-			memset(dp, 0, sizeof(dp));
+		memset(dp, 0, sizeof(dp));
 
-			for(int i = 0; i < n; i++){
-				for(int j = 0; j <= limit; j++){
-					if (j < w[i]) dp[i+1][j] = dp[i][j];
-					else dp[i+1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
-				}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j <= limit; j++){
+				if (j < w[i]) dp[i+1][j] = dp[i][j];
+				else dp[i+1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
 			}
+		}
 
-			cout << dp[n][limit] << "\n";
+		cout << dp[n][limit] << "\n";
 	}
 	
 }
